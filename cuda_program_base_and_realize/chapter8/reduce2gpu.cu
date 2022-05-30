fc2d6e82e65e58d67d050a8c040e90e2e1403b76
lei <dcsvgwudi@gmail.com>
2022-05-30 21:05:48 +0800

    0530

--- cuda_program_base_and_realize/chapter8/reduce2gpu.cu
+++ cuda_program_base_and_realize/chapter8/reduce2gpu.cu
@@ -1,4 +1,8 @@
 #include "../real.cuh"
+
+const int TILE_DIM = 32;
+const int N = 128;
+
 void __global__ reduce_global(real* d_x, real* d_y)
 {
     /**
@@ -26,5 +30,51 @@ void __global__ reduce_global(real* d_x, real* d_y)
     if (tid == 0)
     {
         dy[blockIdx.x] = x[0]; // threadblock的结果放在自己blockIdx对应的位置内
+    }
+}
+
+
+void __global__ reduce_shared(real* d_x, real* d_y)
+{
+    const int tid = threadIdx.x;
+    const int bid = blockIdx.x;
+    const int n = bid * blockDim.x + tid;
+    __shared__ real s_y[128];
+    s_y[tid] = (n < N) ? d_x[n] : 0.0;
+    __syncthreads();
+    for (int offset = blockDim.x >> 1; offset > 0; offset  >>= 1)
+    {
+        if (tid < offset)
+        {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncthreads();
+    }
+    if (tid == 0)
+    {
+        d_y[bid] = s_y[0];
+    }
+}
+
+//bank冲突
+void __global__ void transpose1(const real* A, const real* B, const int N) // N 128
+{
+    //__shared__ real S[TILE_DIM][TILE_DIM]; // TILE_DIM值为32
+    __shared__ real S[TILE_DIM][TILE_DIM + 1]; //解决上面一行的共享bank冲突
+    int bx = blockIdx.x * TILE_DIM;
+    int by = blockidx.y * TILE_DIM;
+
+    int nx1 = bx + threadIdx.x;
+    int ny1 = by + threadIdx.y;
+    if (nx1 < N && ny1 < N)
+    {
+        S[threadIdx.y][threadIdx.x] = A[ny1 * N + nx1]; // 11行 无bank冲突
+    }
+    __syncthreads();
+    int nx2 = bx + threadIdx.y;
+    int ny2 = by = threadIdx.x;
+    if (nx2 < N && ny2 < N)
+    {
+        B[nx2 +* N + ny2] = S[threadIdx.x][threadIdx.y]; // 19行 y方向bank冲突
     }
 }
